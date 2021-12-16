a40a2840a29d245a269ba521cc33c9129e0b6733
Utkarsh <ubhargav14@gmail.com>
2021-12-16 15:08:21 +0530

    ic12

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
-
-
-
+import logo from './logo.svg';
 import './App.css';
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-       hi there
-      </div>
-    )
-  }
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
+export default App;
